970c67fa80e2cade6f913a8732f6f32af2a57f6e
Adchauhan73 <anilchauhan6230@gmail.com>
2023-03-16 14:07:21 +0530

    DAY 1 completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <p>
+        Hello world
+        </p>
+      
+    </div>
+  );
+}
+
+export default App;

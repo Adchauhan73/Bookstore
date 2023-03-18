59042d65063783442677ba834e1a847185526db0
Adchauhan073 <anilchauhan6230@gmail.com>
2023-03-19 00:24:34 +0530

    Day 4-5 completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react'
 // import Home from './page/Home';
-import Title from './components/Title';
+// import Title from './components/Title';
+import Home from './pages/Home';
+import About from './pages/About'
+// import Counter from './components/Counter'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        {/* <Title name="Title1"/>
+
+
+import React from 'react'
+
+function App() {
+  const [page, setPage] = React.useState("Home");
+
+  const handleChange = () => {
+    if (page === "Home") {
+      setPage("About");
+    } else {
+      setPage("Home");
+    }
+  };
+  return (
+    <div>
+      {page === "Home" ? <Home/> : <About/>}
+    <button onClick={handleChange}>Change Page</button>
+      {/* <Title name="Title1"/>
         <Title name="Title2"/>
         <Title  name="Title3"/> */}
         {/* <Home />
-         */}
+        
          <Title title="Anil"desc="is a Developer"/>
-      </div>
-    )
-  }
+         <Home /> */}
+         
+         {/* <Counter /> */}
+         {/* <List /> */}
+
+    </div>
+  )
 }
+
+export default App
